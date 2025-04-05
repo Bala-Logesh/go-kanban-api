e9a02ea67c816a8a7550456ea58dc013291d3b47
Bala Logesh S <balassmp@gmail.com>
2025-04-05 12:20:21 -0400

    Added middleware

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/bala-logesh/go-kanban-api
 
 go 1.24.0
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require (
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/justinas/alice v1.2.0 // indirect
+)
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/justinas/alice"
 )
 
 type RouteResponse struct {
@@ -22,14 +23,21 @@ func main() {
 	url := fmt.Sprintf("127.0.0.1:%d", port)
 
 	log.Println("Setting up routes")
-	router.HandleFunc("/", handleRoot).Methods("GET")
-	router.HandleFunc("/register", handleRegister).Methods("POST")
-	router.HandleFunc("/login", handleLogin).Methods("POST")
-	router.HandleFunc("/projects", handleGetProjects).Methods("GET")
-	router.HandleFunc("/projects/{id}", handleGetProject).Methods("GET")
-	router.HandleFunc("/projects", handleCreateProject).Methods("POST")
-	router.HandleFunc("/projects/{id}", handleUpdateProject).Methods("PUT")
-	router.HandleFunc("/projects/{id}", handleDeleteProject).Methods("DELETE")
+	router.Handle("/", alice.New(loggingMiddleware).ThenFunc(handleRoot)).Methods("GET")
+
+	router.Handle("/register", alice.New(loggingMiddleware).ThenFunc(handleRegister)).Methods("POST")
+
+	router.Handle("/login", alice.New(loggingMiddleware).ThenFunc(handleLogin)).Methods("POST")
+
+	router.Handle("/projects", alice.New(loggingMiddleware).ThenFunc(handleGetProjects)).Methods("GET")
+
+	router.Handle("/projects/{id}", alice.New(loggingMiddleware).ThenFunc(handleGetProject)).Methods("GET")
+
+	router.Handle("/projects", alice.New(loggingMiddleware).ThenFunc(handleCreateProject)).Methods("POST")
+
+	router.Handle("/projects/{id}", alice.New(loggingMiddleware).ThenFunc(handleUpdateProject)).Methods("PUT")
+
+	router.Handle("/projects/{id}", alice.New(loggingMiddleware).ThenFunc(handleDeleteProject)).Methods("DELETE")
 
 	log.Printf("Server running on %v\n", url)
 	log.Fatal(http.ListenAndServe(url, router))
@@ -86,8 +94,18 @@ func handleDeleteProject(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(RouteResponse{Message: "Hello from delete project", ID: id})
 }
 
+// Helpers
 func getIDFromRoute(r *http.Request) string {
 	idStr := mux.Vars(r)["id"]
 
 	return idStr
+}
+
+// Middlewares
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+
+		next.ServeHTTP(w, r)
+	})
 }
